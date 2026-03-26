df3b168d414da605c669015f94ac174208cbee4b
Titansforge <44670193+Titansforge@users.noreply.github.com>
2026-03-26 21:53:28 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
+provider "null" {}
+provider "random" {}
+
+resource "random_pet" "name" {
+  length = 2
+}
+
+resource "null_resource" "example" {
+  triggers = {
+    name = random_pet.name.id
+  }
+
+  provisioner "local-exec" {
+    command = "echo Created resource with name ${random_pet.name.id}"
+  }
+}
